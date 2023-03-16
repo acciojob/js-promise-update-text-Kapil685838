002270a6601f7dca2d9a81baebfd147af29f33fa
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-03-16 15:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function Hello(){
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			resolve("Hello, world!");
-		},1000);
+		}, 1000);
 	});
 }
 
 Hello().then((data) => {
-   document.getElementById("output").textContent = data;
+   output.textContent = data;
 });
